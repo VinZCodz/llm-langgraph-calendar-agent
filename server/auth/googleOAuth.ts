26843d4877a429dca8767c11d0445a29e89c49c6
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-09-15 17:54:17 +0000

    Google auth setup.

---
+++ server/auth/googleOAuth.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import { google } from 'googleapis';
+
+import url from 'url'
+
+const app = express();
+
+const port = 3600;
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.AUTH_CALLBACK_ROUTE
+);
+
+app.get('/authorizeapp', async (req, res) => {
+    const scopes = [
+        'https://www.googleapis.com/auth/calendar'
+    ];
+
+    const authorizationUrl = oauth2Client.generateAuthUrl({
+        access_type: 'online', //set 'offline' if need long time access.
+        prompt: 'consent',
+        scope: scopes,
+        include_granted_scopes: true,
+    });
+
+    res.redirect(authorizationUrl);
+});
+
+app.get('/oauth2callback', async (req, res) => {
+    let q = url.parse(req.url, true).query;
+
+    if (q.error) {
+        console.log('Error:' + q.error);
+    } else {
+        let { tokens } = await oauth2Client.getToken(q.code as string);
+        console.log(tokens);
+        //oauth2Client.setCredentials(tokens);
+    }
+    res.send("Connection established! Please close this tab.")
+});
+
+app.listen(port, () => {
+    console.log(`Auth Server up and running at port: ${port}`)
+});
